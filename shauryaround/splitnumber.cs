d55a055b9ae435520598d4fd3e7f8b30d29ac2df
priyanka <ubhepriyanka243@gmail.com>
2022-08-19 11:51:28 +0530

    commit

---
+++ shauryaround/splitnumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Practice.shauryaround
+{
+    public class splitnumber
+    {
+        static void Main(string[] args)
+        {
+            string s = "10,20,30,40,50,60";
+            string[] s1 = s.Split(",");
+            int c = 0;
+            int sqr = 0;
+            for (int i = 0; i < s1.Length; i++)
+            {
+
+                Console.WriteLine(sqr);
+                c++;
+            }
+            Console.WriteLine(c);
+
+            for (int j = 0; j < s1.Length; j++)
+            {
+                // sqr = s1[j] * s1[j];
+                Console.Write(sqr);
+            }
+        }
+    }
+
+}
---
+++ shauryaround/square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_Practice.shauryaround
+{
+    public class square
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = { 10, 20, 30, 40, 50, 60 };
+            int cnt = 0;
+            for (int j = 0; j < arr.Length; j++)
+            {
+
+                Console.Write(arr[j] + ",");
+
+            }
+            Console.WriteLine();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                cnt++;
+                Console.Write(arr[i] * arr[i] + ",");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Count=" + cnt);
+        }
+    }
+}
+
